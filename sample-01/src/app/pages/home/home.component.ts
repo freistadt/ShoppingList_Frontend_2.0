b19649dc5665ade10025afbbb092a9ab5dadbf75
lukas-zip <Tobi2020!>
2021-05-30 11:37:23 +0200

    simplified first post method

--- sample-01/src/app/pages/home/home.component.ts
+++ sample-01/src/app/pages/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpParams} from '@angular/common/http';
 import { map } from 'rxjs/operators';
 
 @Component({
@@ -13,6 +13,13 @@ export class HomeComponent implements OnInit{
   profileJson: string = null;
   back: string;
   public list: any;
+  posteId: boolean;
+
+  postData = {
+    test: 'test',
+  };
+
+  url = 'http://localhost:8093/addFood';
 
   constructor(public auth: AuthService, private client: HttpClient) {}
 
@@ -21,19 +28,46 @@ export class HomeComponent implements OnInit{
   }
   sendLoggedUser(user: string) {
       this.client.get('http://localhost:8093/getTestString?name=Test').subscribe(data => {
-      this.back = data[0];
+      this.back = data[0][3];
     });
   }
   public getPersonsList(): any{
-    return this.client.get('http://localhost:8093/getTestString?name=Test').subscribe( data => {
+    this.client.get('http://localhost:8093/getTestString?name=Test').subscribe( data => {
       this.list = data;
     });
   }
 
   // Test delte maybe
-  public getList(): any{
-    return this.client.get('http://localhost:8093/getList?name=Onioin').subscribe( data => {
+  public getList(): any {
+    return this.client.get('http://localhost:8093/getList?name=Onioin').subscribe(data => {
       this.list = data;
+    });
+  }
+
+
+    /*
+        this.client.post<any>('http://localhost:8093/addFood', { name: 'Onion', food: 'Test' }).subscribe(data => {
+      this.posteId = data;
+    });
+        console.log('There was an error!');
+    return this.client.post<any>('http://localhost:8093/getList?name=Onioin&food=Test').subscribe(data => {
+      this.posteId = data;
+    });
+
+     this.client.post('http://localhost:8093/getList', {body}).subscribe(data => {
+        alert('ok');
+    });
+  }
+  */
+
+  public postTest(): any{
+    let body = new HttpParams();
+    body = body.set('food', 'Test');
+    body = body.set('name', 'Onioin');
+
+    console.log(body.toString());
+    this.client.post(this.url, body).toPromise().then((data: any) => {
+      console.log(data);
     });
   }
 
